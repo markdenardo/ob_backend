2578ee52af67968aee8dcec08ee07ffec9ccb384
markdenardo <mark.denardo@gmail.com>
2020-01-06 14:53:15 -0500

    functioning object api

--- oblique_api/app/controllers/notes_controller.rb
+++ oblique_api/app/controllers/notes_controller.rb
@@ -1,20 +1,19 @@
 class NotesController < ApplicationController
 
-  # def index
-  #   notes = Note.all
-  #   render json: notes
-  #   # render json: notes, only: [:comment, :like, :card_id]
-  # end
-  #
-  # def show
-  #   note = Note.find_by(id: params[:id])
-  #   if note
-  #     render json note
-  #     # render json: note, only: [:comment, :like, :card_id]
-  #   else
-  #     render json: { message: 'Note not found' }
-  #   end
-  # end
+  def index
+    notes = Note.all
+    render json: notes
+    # render json: notes, only: [:url, :like, :note_id]
+  end
 
+  def show
+    note = Note.find_by(id: params[:id])
+    if note
+      render json: note
+      # render json: note, only: [:url, :like, :note_id]
+    else
+      render json: { message: 'Note not found' }
+    end
+  end
 
 end
